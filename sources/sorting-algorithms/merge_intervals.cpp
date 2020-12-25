3ce542f5fd4b97bbea7cf6c5d23ad9122d79557f
Vlad <vladadgad@gmail.com>
2020-12-25 23:48:13 +0100

    refactor: merge_intervals

--- sources/sorting-algorithms/merge_intervals.cpp
+++ sources/sorting-algorithms/merge_intervals.cpp
@@ -5,8 +5,6 @@ std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals) {
 		return i1[0] > i2[0];
 	});
 
-	std::vector<std::vector<int>> a;
-
 	for (unsigned int i = intervals.size() - 1; i > 0; --i) {
 		if (intervals[i][1] >= intervals[i - 1][0]) {
 			if (intervals[i][1] < intervals[i - 1][1]) {
