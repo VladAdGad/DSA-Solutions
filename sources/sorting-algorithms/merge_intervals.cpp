0a71cb207fc730b87724d598cc2c40e9db0bca05
Vlad <vladadgad@gmail.com>
2020-12-25 20:22:28 +0100

    solution: faster merge_intervals

--- sources/sorting-algorithms/merge_intervals.cpp
+++ sources/sorting-algorithms/merge_intervals.cpp
@@ -2,18 +2,19 @@
 
 std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals) {
 	std::sort(intervals.begin(), intervals.end(), [](const auto& i1, const auto& i2) {
-		return i1[0] < i2[0];
+		return i1[0] > i2[0];
 	});
 
-	for (int i = 0; i < intervals.size() - 1; ++i) {
-		if (intervals[i][1] >= intervals[i + 1][0]) {
-			if (intervals[i][1] < intervals[i + 1][1]) {
-				intervals[i] = std::vector{intervals[i][0], intervals[i + 1][1]};
+	std::vector<std::vector<int>> a;
+
+	for (int i = intervals.size() - 1; i > 0; --i) {
+		if (intervals[i][1] >= intervals[i - 1][0]) {
+			if (intervals[i][1] < intervals[i - 1][1]) {
+				intervals[i] = {intervals[i][0], intervals[i - 1][1]};
 			} else {
-				intervals[i] = std::vector{intervals[i][0], intervals[i][1]};
+				intervals[i] = {intervals[i][0], intervals[i][1]};
 			}
-			intervals.erase(intervals.begin() + i + 1);
-			--i;
+			intervals.erase(intervals.begin() + i - 1);
 		}
 	}
 
