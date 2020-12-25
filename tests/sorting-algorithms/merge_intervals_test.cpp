1dc79d0e9993027350826f5e46a74ee27ed83570
Vlad <vladadgad@gmail.com>
2020-12-25 20:35:15 +0100

    test: fix merge_intervals

--- tests/sorting-algorithms/merge_intervals_test.cpp
+++ tests/sorting-algorithms/merge_intervals_test.cpp
@@ -4,7 +4,7 @@
 
 TEST_CASE("Merge Intervals", "[Sorting Algorithms]") {
 	std::vector<std::vector<int>> intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
-	REQUIRE_THAT(merge(intervals), Catch::Matchers::Equals(std::vector<std::vector<int>>{{1, 6}, {8, 10}, {15, 18}}));
+	REQUIRE_THAT(merge(intervals), Catch::Matchers::Equals(std::vector<std::vector<int>>{{15, 18}, {8, 10}, {1, 6}}));
 
 	std::vector<std::vector<int>> intervals1 = {{1, 4}, {0, 2}, {3, 5}};
 	REQUIRE_THAT(merge(intervals1), Catch::Matchers::Equals(std::vector<std::vector<int>>{{0, 5}}));
