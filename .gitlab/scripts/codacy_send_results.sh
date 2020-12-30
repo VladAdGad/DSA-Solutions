d0cc3afd197d5309ced5cebd7a5accaf95ebb6a3
Vlad <vladadgad@gmail.com>
2020-12-30 21:47:16 +0100

    try to integrate Codacy

---
+++ .gitlab/scripts/codacy_send_results.sh
@@ -0,0 +1,8 @@
+./codacy-clang-tidy-${CODACY_CLANG_TIDY_VERSION} |
+  curl -XPOST -L -H "project-token: ${CODACY_PROJECT_TOKEN}" \
+    -H "Content-type: application/json" -d @- \
+    "https://api.codacy.com/2.0/commit/${CI_COMMIT_REF_NAME}/issuesRemoteResults"
+
+curl -XPOST -L -H "project-token: ${CODACY_PROJECT_TOKEN}" \
+  -H "Content-type: application/json" \
+  "https://api.codacy.com/2.0/commit/${CI_COMMIT_REF_NAME}/resultsFinal"
